692aa4debc2c8f81220a35dee6f2845ea6595fe7
Garvit Singla <garvits093@gmail.com>
2026-06-30 20:11:08 +0530

    Bubble sort Problem

---
+++ C_Programs/BubbleSort.c
@@ -0,0 +1,77 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Generates an array of random integers of given size.
+int *generateArray(int size) {
+  int *arr = malloc(size * sizeof(int));
+  for (int i = 0; i < size; i++) {
+    arr[i] = rand() % 100;
+  }
+  return arr;
+};
+
+// Sorts the given array using Bubble Sort.
+void applyBubbleSort(int arr[], int size) {
+  for (int i = 0; i < size - 1; i++) {
+
+    for (int j = 0; j < size - i - 1; j++) {
+
+      if (arr[j + 1] < arr[j]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+};
+
+// Returns true if the array is sorted, otherwise false.
+bool verifySort(int arr[], int size) {
+
+  bool flag = true;
+  for (int i = 1; i < size; i++) {
+    if (arr[i - 1] > arr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+int main() {
+  int size;
+
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+
+  // Generate random array
+  int *arr = generateArray(size);
+
+  // Start timer
+  clock_t start = clock();
+
+  // Apply sorting algorithm
+  applyBubbleSort(arr, size);
+
+  // Stop timer
+  clock_t end = clock();
+
+  // Calculate execution time
+  double executionTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+  // Verify correctness
+  if (verifySort(arr, size)) {
+    printf("Array is sorted successfully.\n");
+  } else {
+    printf("Sorting failed!\n");
+  }
+
+  printf("Execution Time: %.6f seconds\n", executionTime);
+  printf("Execution Time: %.3f milliseconds\n", executionTime * 1000);
+
+  // Free dynamically allocated memory
+  free(arr);
+
+  return 0;
+}
