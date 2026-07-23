42ce432af5ba82753cc745750ffeb05855098b96
Garvit Singla <garvits093@gmail.com>
2026-07-23 18:10:06 +0530

    Assignment 4: Completed count inversions

---
+++ Assignments/Assignment4/CountInversions/countInversion.cpp
@@ -0,0 +1,62 @@
+#include "iostream"
+#include "vector"
+
+void merge(std::vector<int> &arr,int low,int mid,int high){
+    int left = low,right=mid+1;
+
+    std::vector<int> temp(high-low+1);
+    int k = 0;
+    while(left <= mid && right <= high){
+        if(arr[left]<=arr[right]){
+            temp[k++] = arr[left++];
+        }else{
+            temp[k++] = arr[right++];
+        }
+    }
+
+    while(left<=mid){
+        temp[k++] = arr[left++];
+    }
+    while(right <= high){
+        temp[k++] = arr[right++];
+    }
+
+    k = 0;
+    for(int i = low ; i<= high ; i++){
+        arr[i] = temp[k++];
+    }
+
+}
+int countInversion(std::vector<int> &arr,int low,int mid,int high){
+    int right = mid+1;
+
+    int count = 0;
+    for(int i = low ; i <= mid ; i++){
+        while(right <= high && arr[right] < arr[i])right++;
+        count += (right-(mid+1));
+    }
+    return count;
+}
+int mergeSort(std::vector<int>& arr,int low,int high){
+    if(low >= high) return 0;
+
+    int mid = low+(high-low)/2;
+
+    int l = mergeSort(arr,low,mid);
+    int r = mergeSort(arr,mid+1,high);
+    int c = countInversion(arr,low,mid,high);
+    merge(arr,low,mid,high);
+    return l+r+c;
+}
+int main(){
+    int n;
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+    for(int i =0 ; i < n ; i++) std::cin >> arr[i];
+
+    std::cout << mergeSort(arr,0,n-1) << std::endl ;
+
+
+
+}
