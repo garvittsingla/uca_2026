1d05137bfee8a4d670a8f3ecc79f296fb940817b
Garvit Singla <garvits093@gmail.com>
2026-08-03 12:05:50 +0530

    Week 4 : bitwise bang assignemnt comeplete

--- Assignments/Assignment5/bang.cpp
+++ Assignments/Assignment5/bang.cpp
@@ -8,14 +8,15 @@
  *   Rating: 4 
  */
 int bang(int x) {
-    int negX = ~x + 1;
-    int sign = negX | x;
-    int signedBit = sign >> 31;
-    return signedBit+1;
+   int twocomplement = ~x+1;
+   int complementSignedBit = twocomplement >> 31;
+   int  signedBit = x >> 31;
+   return ((signedBit | complementSignedBit)) + 1;
 }
 
 
 int main(){
     std::cout << bang(3) << std::endl; //0
     std::cout << bang(0) << std::endl; //1
+    std::cout << bang(-1) << std::endl; //0
 }
