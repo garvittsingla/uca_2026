c94e435f1c4bd30c5d8b0ec71e392d54d19e12e8
Garvit Singla <garvits093@gmail.com>
2026-08-03 09:52:37 +0530

    Week 4 : Get bytes assignment done

---
+++ Assignments/Assignment5/getByte.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+/* 
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByte(0x12345678,1) = 0x56
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+
+int getByte(int x, int n) {
+    int shiftNumber = n << 3;
+    int mask = 0xFF << shiftNumber;
+    return ((x & mask) >> shiftNumber) & 0xFF;
+}
+
+int main(){
+    std::cout << std::hex << getByte(0x12345678,1) << std::endl;
+}
